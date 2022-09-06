9a102c3ad54ded0328b7aa31956551e0f146e059
DESKTOP-KC8155E\Lenovo <EasterEggs1230@gmail.com>
2022-09-06 17:07:44 +0700

    Edit OnCollisionStay2D

--- RedRunner-master/Assets/Scripts/RedRunner/Enemies/Spike.cs
+++ RedRunner-master/Assets/Scripts/RedRunner/Enemies/Spike.cs
@@ -24,19 +24,36 @@ namespace RedRunner.Enemies
 		void OnCollisionStay2D (Collision2D collision2D)
 		{
 			Character character = collision2D.collider.GetComponent<Character> ();
+
 			if (character && !character.IsDead.Value) {
 				bool isTop = false;
 				ContactPoint2D mainPoint;
-				for (int i = 0; i < collision2D.contacts.Length; i++) {
-					if (!isTop) {
-						isTop = collision2D.contacts [i].normal.y < -0.7f && collision2D.contacts [i].normal.y >= -1f;
-					} else {
-						break;
-					}
-				}
+
+				CheckDistanceBetween();
+
 				if (isTop) {
 					Kill (character);
 				}
+			}
+		}
+
+		void CheckDistanceBetween()
+        {
+			for (int i = 0; i < collision2D.contacts.Length; i++)
+			{
+				CheckTop();
+			}
+		}
+
+		void CheckTop()
+        {
+			if (!isTop)
+			{
+				isTop = collision2D.contacts[i].normal.y < -0.7f && collision2D.contacts[i].normal.y >= -1f;
+			}
+			else
+			{
+				break;
 			}
 		}
 
